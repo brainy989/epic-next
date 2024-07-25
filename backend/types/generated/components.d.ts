912d350480b4219cc039c36378741f072f6c871f
Alex Jones <alexjones11103@outlook.com>
2024-07-25 06:33:30 -0400

    setting up on github

---
+++ backend/types/generated/components.d.ts
@@ -0,0 +1,88 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface LayoutHeroSection extends Schema.Component {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+    description: '';
+  };
+  attributes: {
+    image: Attribute.Media<'images'>;
+    heading: Attribute.String;
+    subHeading: Attribute.Text;
+    link: Attribute.Component<'components.link'>;
+  };
+}
+
+export interface LayoutHeader extends Schema.Component {
+  collectionName: 'components_layout_headers';
+  info: {
+    displayName: 'Header';
+    description: '';
+  };
+  attributes: {
+    logoText: Attribute.Component<'components.link'>;
+    ctaButton: Attribute.Component<'components.link'>;
+  };
+}
+
+export interface LayoutFooter extends Schema.Component {
+  collectionName: 'components_layout_footers';
+  info: {
+    displayName: 'Footer';
+  };
+  attributes: {
+    logoText: Attribute.Component<'components.link'>;
+    text: Attribute.String;
+    socialLink: Attribute.Component<'components.link', true>;
+  };
+}
+
+export interface LayoutFeaturesSection extends Schema.Component {
+  collectionName: 'components_layout_features_sections';
+  info: {
+    displayName: 'Features Section';
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.Text;
+    feature: Attribute.Component<'components.feature', true>;
+  };
+}
+
+export interface ComponentsLink extends Schema.Component {
+  collectionName: 'components_components_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    url: Attribute.String;
+    text: Attribute.String;
+    isExternal: Attribute.Boolean & Attribute.DefaultTo<false>;
+  };
+}
+
+export interface ComponentsFeature extends Schema.Component {
+  collectionName: 'components_components_features';
+  info: {
+    displayName: 'Feature';
+  };
+  attributes: {
+    heading: Attribute.String;
+    subHeading: Attribute.Text;
+    icon: Attribute.Enumeration<['CLOCK_ICON', 'CHECK_ICON', 'CLOUD_ICON']>;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'layout.hero-section': LayoutHeroSection;
+      'layout.header': LayoutHeader;
+      'layout.footer': LayoutFooter;
+      'layout.features-section': LayoutFeaturesSection;
+      'components.link': ComponentsLink;
+      'components.feature': ComponentsFeature;
+    }
+  }
+}
---
+++ frontend/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,5 @@
+import { SignupForm } from "@/components/forms/SignupForm";
+
+export default function SignInRoute() {
+  return <SignupForm />;
+}
